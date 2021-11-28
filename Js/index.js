3e61454c0191a74deeaec9c1a06837b45732dc37
MeqdamAlqudah <memuq56@gmail.com>
2021-11-28 17:22:57 +0200

    Delete prettier comments

--- Js/index.js
+++ Js/index.js
@@ -1,8 +1,8 @@
 // more-button activation
 const moreElements = function () {
-  // prettier-ignore
+  
   const hide = document.querySelectorAll('.hide');
-  // prettier-ignore
+  
   if (hide[0].classList.contains('hidden')) {
     for (let i = 0; i < hide.length; i += 1) {
       hide[i].classList.remove('hidden');
@@ -19,23 +19,23 @@ const moreElements = function () {
     document.querySelector('.more-btn .text').textContent = 'MORE';
   }
 };
-// prettier-ignore
+
 document
   .querySelector('.more-btn')
   .addEventListener('click', moreElements);
 
 //  The menu
 // Open the menu
-// prettier-ignore
+
 const openMenu = function () {
   document.querySelector('.menu.mobile').classList.remove('hidden');
 };
 // Close the menu
-// prettier-ignore
+
 const closeMenu = function () {
   document.querySelector('.menu.mobile').classList.add('hidden');
 };
-// prettier-ignore
+
 const menuOpenerCloser = function () {
   if (document.getElementsByClassName('menu')[0].classList.contains('hidden')) {
     openMenu();
@@ -43,15 +43,15 @@ const menuOpenerCloser = function () {
     closeMenu();
   }
 };
-// prettier-ignore
+
 document.getElementsByClassName('menu_')[0].addEventListener('click', menuOpenerCloser);
-// prettier-ignore
+
 document
   .getElementsByClassName('cancel')[0]
   .addEventListener('click', menuOpenerCloser);
 
 // Show Home Page
-// prettier-ignore
+
 const showHome = function () {
   document.querySelector('.text-container').classList.remove('hidden');
   document.querySelector('.text-containerAbout-page').classList.add('hidden');
@@ -87,7 +87,7 @@ const showHome = function () {
   closeMenu();
 };
 // Show About Page
-// prettier-ignore
+
 const showAbout = function () {
   document.querySelector('.text-container').classList.add('hidden');
   document.querySelector('.LogoImageContainer').style.height = '36vmax';
@@ -127,21 +127,20 @@ const showAbout = function () {
   closeMenu();
 };
 // add Event listener to menu mobile options
-// prettier-ignore
+
 document.querySelector('.menu li.about').addEventListener('click', showAbout);
 
-// prettier-ignore
+
 document.querySelector('.menu li.home').addEventListener('click', showHome);
 
 // add Event listener to menu Desktop options
-// prettier-ignore
+
 document.querySelector('.menu .menuDesk li.about').addEventListener('click', showAbout);
 
-// prettier-ignore
+
 document.querySelector('.menu .menuDesk li.home').addEventListener('click', showHome);
-// prettier-ignore
+
 document.querySelector('.LogoDesktop').addEventListener('click', showHome);
-// prettier-ignore
 window.addEventListener('resize',() =>{
   
   if( document.getElementsByClassName('about-page')[0].classList.contains('hidden')){
@@ -164,7 +163,6 @@ const changeThings = function () {
     document.querySelector('.text-containerAbout-page').style.width = '90%';
   }
 };
-// prettier-ignore
 document.querySelector('body').addEventListener('scroll', changeThings);
 
 // Fix it
